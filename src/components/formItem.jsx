13257f72f72ace2c53791b423c536b8d67d89707
Altaf Parves <altafparvesh@gmail.com>
2025-01-22 23:20:34 +0800

    fix form item

--- src/components/formItem.jsx
+++ src/components/formItem.jsx
@@ -5,8 +5,14 @@ export default function FormItem({ label, placeholder, onChange, inputRef, rows 
   return (
     <div className="form w-full flex-col flex items-start" {...props}>
       <label className="text-black text-14-700 mb-[8px]">{label}</label>
-      <textarea rows={rows} ref={inputRef} onChange={onChange} placeholder={placeholder} type="text" className="appearance-none rounded-[12px] w-full bg-secondary p-[14px] text-grey leading-tight focus:outline-none focus:shadow-outline" />
-      
+      <textarea
+        rows={rows}
+        ref={inputRef}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        type="text"
+        className="appearance-none rounded-[12px] w-full bg-secondary p-[14px] text-grey leading-tight focus:outline-none focus:shadow-outline"
+      />
     </div>
   );
 }
