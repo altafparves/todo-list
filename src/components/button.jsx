dad35280729a7529534c6ad03ad6a1c9099c143c
Altaf Parves <altafparvesh@gmail.com>
2025-01-22 23:20:20 +0800

    feat: button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,3 +1,3 @@
-export default function Button({ children, className }) {
-  return <button className={`w-full rounded-full p-[14px] bg-black flex items-center justify-center text-14-700 text-base ${className}`}>{children}</button>;
+export default function Button({onClick, children, className }) {
+  return <button onClick={onClick} className={`w-full rounded-full p-[14px] bg-black flex items-center justify-center text-14-700 text-base ${className}`}>{children}</button>;
 }
