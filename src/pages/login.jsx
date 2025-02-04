debe1fd7c4d944db42a54ac00201ebf13922f9bb
Altaf Parves <altafparvesh@gmail.com>
2025-02-04 22:47:48 +0800

    fix[login]: error text color

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -60,10 +60,10 @@ export default function Login() {
             </button>
           </p>
           <div className="form w-full flex flex-col gap-[16px]">
-            <FormItem label="Username" onChange={handleEmailChange} placeholder="Enter your email" />
+            <FormItem label="Email" onChange={handleEmailChange} placeholder="Enter your email" />
             <PasswordForm label="Password" onChange={handlePasswordChange} placeholder="Enter your password" />
             <div className="flex flex-col gap-[8px] w-full">
-              {error && <p className="mt-[3px] pl-[14px] text-11-400 text-red-500">{error}</p>}
+              {error && <p className="mt-[3px] pl-[14px] text-11-400 text-red">{error}</p>}
               <Button className="mt-[16px]" onClick={handleSubmit}>
                 Login
               </Button>
